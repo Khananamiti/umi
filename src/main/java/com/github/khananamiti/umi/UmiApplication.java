06d5528194751a4a78897c2383394c72541eb442
khananamiti <nuralinova_kha@mail.ru>
2023-01-15 00:35:26 +0600

    spring boot app added

---
+++ src/main/java/com/github/khananamiti/umi/UmiApplication.java
@@ -0,0 +1,13 @@
+package com.github.khananamiti.umi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmiApplication.class, args);
+	}
+
+}
